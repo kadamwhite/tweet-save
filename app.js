27d14fce80f6fc50f7ab7b7bd9fec3d32966a84d
Tyler Kellen <tyler@sleekcode.net>
2014-02-17 12:02:05 -0500

    simplify

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const HASHTAG = '#cat';
-
-const twitter = require('ntwitter');
 const config = require('./config');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('tweets.sqlite3');
+const twitter = require('ntwitter');
+const sqlite3 = require('sqlite3');
 
-db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='tweets'", function (err, name) {
-  if(!name) {
-    db.run("CREATE TABLE tweet (hashtag TEXT, data TEXT)");
-  }
-});
+const DB = new sqlite3.Database('tweets.sqlite3');
+
+DB.run("CREATE TABLE IF NOT EXISTS tweet (hashtag TEXT, data TEXT)");
 
 var twit = new twitter(config);
 
@@ -21,7 +17,7 @@ twit.verifyCredentials(function (err, data) {
     twit.stream('statuses/filter', {'track': [HASHTAG]}, function (stream) {
       stream.on('data', function (data) {
         console.log('Saving tweet for '+HASHTAG+'...');
-        db.run('INSERT INTO tweet (hashtag, data) VALUES (?, ?)', [
+        DB.run('INSERT INTO tweet (hashtag, data) VALUES (?, ?)', [
           HASHTAG,
           JSON.stringify(data)
         ])
@@ -29,4 +25,3 @@ twit.verifyCredentials(function (err, data) {
     });
   }
 });
-
